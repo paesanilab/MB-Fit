6f94a35ff83527d715d30156eec5d58e3705b23c
Marc Riera <mrierari@ucsd.edu>
2017-09-13 18:49:44 -0700

    Added test script for input generation
    Exits with 1 if one test fails

--- polynomial_generation/src/generate_input_poly.py
+++ polynomial_generation/src/generate_input_poly.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 import sys
 import os
 
 
-# In[2]:
+# In[12]:
 
 if len(sys.argv) != 2:
     print("Usage: ./script <input.in>")
@@ -18,27 +18,41 @@ else:
     name = sys.argv[1]
 
 
-# In[8]:
+# In[18]:
 
 # This should be the commandline argument
 #name = "A1B2Z2_D1E2.in"
 #name = "A1B2_A1B2.in"
+#name = "A1B4.in"
 molec = os.path.splitext(name)[0]
 f = open(name, 'w')
+
+# Check how many terms do we have, and act accordingly
+n_body = len(molec.split('_'))
+# Script doesn't handle 3b
+if n_body > 2:
+    print("Script not prepared yet for 3-body inputs")
+    sys.exit(1)
+    
 mon1 = molec.split('_')[0]
-mon2 = molec.split('_')[1]
 f.write('add_molecule[\'' + mon1 + '\']\n')
-f.write('add_molecule[\'' + mon2 + '\']\n')
+
+# If we have two molecules
+if n_body > 1:
+    mon2 = molec.split('_')[1]
+    f.write('add_molecule[\'' + mon2 + '\']\n')
+
 vsites = ['Z','Y','X']
 
 
-# In[9]:
+# In[19]:
 
 types_a = list(mon1)
-types_b = list(mon2)
+if n_body > 1:
+    types_b = list(mon2)
 
 
-# In[10]:
+# In[20]:
 
 # Generating the non linear parameter list
 nlparam = []
@@ -47,14 +61,16 @@ t1 = []
 for i in range(0,len(types_a),2):
     for j in range(int(types_a[i+1])):
         t1.append(types_a[i])
-t2 = []
-# Monomer 2 parameters
-for i in range(0,len(types_b),2):
-    for j in range(int(types_b[i+1])):
-        t2.append(types_b[i])
+
+if n_body > 1:
+    t2 = []
+    # Monomer 2 parameters
+    for i in range(0,len(types_b),2):
+        for j in range(int(types_b[i+1])):
+            t2.append(types_b[i])
 
 
-# In[11]:
+# In[21]:
 
 nc = 0
 set_m1 = []
@@ -70,18 +86,18 @@ for i in range(0,len(types_a),2):
                 set_m1.append(types_a[i] + '_' + str(n) + '_a')
                 n = n + 1
                 nc = nc + 1
-
-for i in range(0,len(types_b),2):
-    n = 1
-    if (int(types_b[i+1]) == 1):
-        if not types_b[i] in vsites:
-            set_m2.append(types_b[i] + '_' +  '_b')
-    else:
-        for j in range(int(types_b[i+1])):
+if n_body > 1:
+    for i in range(0,len(types_b),2):
+        n = 1
+        if (int(types_b[i+1]) == 1):
             if not types_b[i] in vsites:
-                set_m2.append(types_b[i] + '_' + str(n) + '_b')
-                n = n + 1
-                nc = nc + 1
+                set_m2.append(types_b[i] + '_' +  '_b')
+        else:
+            for j in range(int(types_b[i+1])):
+                if not types_b[i] in vsites:
+                    set_m2.append(types_b[i] + '_' + str(n) + '_b')
+                    n = n + 1
+                    nc = nc + 1
             
 for i in range(0,len(types_a),2):
     n = 1
@@ -90,15 +106,16 @@ for i in range(0,len(types_a),2):
             set_m1.append(types_a[i] + '_' + str(n) + '_a')
             n = n + 1
 
-for i in range(0,len(types_b),2):
-    n = 1
-    for j in range(int(types_b[i+1])):
-        if types_b[i] in vsites:
-            set_m2.append(types_b[i] + '_' + str(n) + '_b')
-            n = n + 1
+if n_body > 1:
+    for i in range(0,len(types_b),2):
+        n = 1
+        for j in range(int(types_b[i+1])):
+            if types_b[i] in vsites:
+                set_m2.append(types_b[i] + '_' + str(n) + '_b')
+                n = n + 1
 
 
-# In[12]:
+# In[22]:
 
 f.write('\n')
 # Intramolecular distances:
@@ -110,20 +127,23 @@ for i in range(0,len(set_m1) - 1):
         if not ti[0] in vsites and not tj[0] in vsites:
             f.write('add_variable[\'' + ti[0] + ti[1] + '\', \'' + ti[2] + '\', \''                   + tj[0] + tj[1] + '\', \'' + tj[2] + '\', \'x-intra-' + t + '\']\n')
             
-for i in range(0,len(set_m2) - 1):
-    for j in range(i + 1,len(set_m2)):
-        ti = set_m2[i].split('_')
-        tj = set_m2[j].split('_')
-        t = ''.join(sorted(ti[0] + tj[0]))
-        if not ti[0] in vsites and not tj[0] in vsites:
-            f.write('add_variable[\'' + ti[0] + ti[1] + '\', \'' + ti[2] + '\', \''                   + tj[0] + tj[1] + '\', \'' + tj[2] + '\', \'x-intra-' + t + '\']\n')
+if n_body > 1: 
+
+            
+    for i in range(0,len(set_m2) - 1):
+        for j in range(i + 1,len(set_m2)):
+            ti = set_m2[i].split('_')
+            tj = set_m2[j].split('_')
+            t = ''.join(sorted(ti[0] + tj[0]))
+            if not ti[0] in vsites and not tj[0] in vsites:
+                f.write('add_variable[\'' + ti[0] + ti[1] + '\', \'' + ti[2] + '\', \''                       + tj[0] + tj[1] + '\', \'' + tj[2] + '\', \'x-intra-' + t + '\']\n')
 # Intermolecular distances
-for i in range(0,len(set_m1)):
-    for j in range(0,len(set_m2)):
-        ti = set_m1[i].split('_')
-        tj = set_m2[j].split('_')
-        t = ''.join(sorted(ti[0] + tj[0]))
-        f.write('add_variable[\'' + ti[0] + ti[1] + '\', \'' + ti[2] + '\', \''                   + tj[0] + tj[1] + '\', \'' + tj[2] + '\', \'x-' + t + '\']\n')
+    for i in range(0,len(set_m1)):
+        for j in range(0,len(set_m2)):
+            ti = set_m1[i].split('_')
+            tj = set_m2[j].split('_')
+            t = ''.join(sorted(ti[0] + tj[0]))
+            f.write('add_variable[\'' + ti[0] + ti[1] + '\', \'' + ti[2] + '\', \''                       + tj[0] + tj[1] + '\', \'' + tj[2] + '\', \'x-' + t + '\']\n')
 f.close()
 
 

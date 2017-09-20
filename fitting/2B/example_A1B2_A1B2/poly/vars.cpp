58e3359686fda09087aa5b2e8ee576d7b0a1acf4
Marc Riera <mrierari@ucsd.edu>
2017-09-20 11:37:06 -0700

    [add] test A1B2_A1B2

---
+++ fitting/2B/example_A1B2_A1B2/poly/vars.cpp
@@ -0,0 +1,18 @@
+
+// <-> variables <->
+
+    x[0] = @VAR@-|x-intra-AB|(AaB1a;
+    x[1] = @VAR@-|x-intra-AB|(AaB2a;
+    x[2] = @VAR@-|x-intra-BB|(B1aB2a;
+    x[3] = @VAR@-|x-intra-AB|(AbB1b;
+    x[4] = @VAR@-|x-intra-AB|(AbB2b;
+    x[5] = @VAR@-|x-intra-BB|(B1bB2b;
+    x[6] = @VAR@-|x-AA|(AaAb;
+    x[7] = @VAR@-|x-AB|(AaB1b;
+    x[8] = @VAR@-|x-AB|(AaB2b;
+    x[9] = @VAR@-|x-AB|(B1aAb;
+    x[10] = @VAR@-|x-BB|(B1aB1b;
+    x[11] = @VAR@-|x-BB|(B1aB2b;
+    x[12] = @VAR@-|x-AB|(B2aAb;
+    x[13] = @VAR@-|x-BB|(B2aB1b;
+    x[14] = @VAR@-|x-BB|(B2aB2b;
---
+++ fitting/2B/example_A1B2_A1B2/run_test_A1B2_A1B2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+WORKDIR=$PWD
+../run_example.sh A1B2_A1B2.in $WORKDIR/poly/ $WORKDIR/../template/ $WORKDIR/get_2b_codes.py 

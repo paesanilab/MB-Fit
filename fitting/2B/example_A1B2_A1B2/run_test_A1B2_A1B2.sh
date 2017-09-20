05ea644734b4fc4c25956646e64f254d8f1f8a7f
Marc Riera <mrierari@ucsd.edu>
2017-09-20 11:49:02 -0700

    Added script automatic for testing

--- fitting/2B/example_A1B2_A1B2/run_test_A1B2_A1B2.sh
+++ fitting/2B/example_A1B2_A1B2/run_test_A1B2_A1B2.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 WORKDIR=$PWD
-../run_example.sh A1B2_A1B2.in $WORKDIR/poly/ $WORKDIR/../template/ $WORKDIR/get_2b_codes.py 
+../run_example.sh A1B2_A1B2.in $WORKDIR/poly/ $WORKDIR/../template/ $WORKDIR/../get_2b_codes.py 
+
+if [ $? -eq 0 ]; then
+  echo "Code generation complete and compiled"
+else
+  echo "Code generation failed"
+  exit 1
+fi
+
+cd fit
+../A1B2_A1B2/fit-2b ts_co2_co2.xyz > fit.log
+
+if [ $? -eq 0 ]; then
+  echo "Fit complete"
+else
+  echo "Fit failed"
+  exit 1
+fi
+
+
+

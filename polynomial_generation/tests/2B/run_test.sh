c4c8d51e212b20b7a0d772fc388e61a84270316e
Andreas Goetz <agoetz@sdsc.edu>
2018-03-20 03:15:59 -0700

    Clean up tests

--- polynomial_generation/tests/2B/run_test.sh
+++ polynomial_generation/tests/2B/run_test.sh
@@ -1,30 +1,26 @@
 #!/bin/bash 
 
-./../../src/poly-gen_mb-nrg.pl 2 h2o_2.in > h2o_2.log 
-## Testing (H2O)2
-diff h2o_2.log h2o_2_expected/h2o_2.log  
-exit_code=$?
+generator=$(pwd)/../../src/poly-gen_mb-nrg.pl
+order=2
 
-if [ $exit_code -ne 0 ]
-then
-    echo "(H2O)2 failed!!"
-    exit $exit_code
-else
-    echo "(H2O)2 passed!"
-    rm *log poly* vars.cpp
-fi
+tests="h2o_2 n2o5_h2o"
 
-./../../src/poly-gen_mb-nrg.pl 2 n2o5_h2o.in > n2o5_h2o.log 
-## Testing N2O5-H2O
-diff n2o5_h2o.log n2o5_h2o_expected/n2o5_h2o.log  
-exit_code=$?
+for test in $tests
+do
 
-if [ $exit_code -ne 0 ]
-then
-    echo "N2O5-H2O failed!!"
-    exit $exit_code
-else
-    echo "N2O5-H2O passed!"
-    rm *log poly* vars.cpp
-fi
+    pushd $test > /dev/null
+    $generator $order $test.in > $test.log
+    diff $test.log expected/$test.log
+    exit_code=$?
+    if [ $exit_code -ne 0 ]
+    then
+	echo "$test failed!!"
+	exit $exit_code
+    else
+	echo "$test passed!"
+	rm *log poly* vars.cpp
+    fi
+    popd > /dev/null
+    
+done
 

7a75429435e41b98af5be71acedcfe491e496996
Andreas Goetz <agoetz@sdsc.edu>
2018-04-25 18:32:51 -0700

    Improve test scripts

--- qm_mb_energy_calculator/tests/dimers/test.sh
+++ qm_mb_energy_calculator/tests/dimers/test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 driver=../../src/driver.py
+log=test.log
 
 python3 $driver
 
-ndiff training_set.xyz expected/training_set.xyz > test.log
+ndiff training_set.xyz expected/training_set.xyz > $log
 exit_code=$?
 
-ndiff mbdecomp.log expected/mbdecomp.log >> test.log
+ndiff mbdecomp.log expected/mbdecomp.log >> $log
 exit_code=$(($exit_code + $?))
 
-ndiff -quiet -abserr 1.e-12 -separators '[ \t,()]' json_output.json expected/json_output.json >> test.log
+ndiff -quiet -abserr 1.e-12 -separators '[ \t,()]' json_output.json expected/json_output.json >> $log
 exit_code=$(($exit_code + $?))
 
 if [ $exit_code -ne 0 ]
@@ -19,5 +20,5 @@ then
     exit $exit_code
 else
     echo "test passed!"
-    rm -f timer.dat training_set.xyz mbdecomp.log json_output.json test.log
+    rm -f timer.dat training_set.xyz mbdecomp.log json_output.json $log
 fi

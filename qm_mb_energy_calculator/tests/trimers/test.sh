a7b432ff46a4ca8d54cede5301a9adc3499074b5
Andreas Goetz <agoetz@sdsc.edu>
2018-04-25 18:08:19 -0700

    Add test for multiple trimers

---
+++ qm_mb_energy_calculator/tests/trimers/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+driver=../../src/driver.py
+
+python3 $driver
+
+ndiff training_set.xyz expected/training_set.xyz
+exit_code=$?
+
+ndiff mbdecomp.log expected/mbdecomp.log
+exit_code=$(($exit_code + $?))
+
+ndiff -quiet -abserr 1.e-12 -separators '[ \t,()]' json_output.json expected/json_output.json
+exit_code=$(($exit_code + $?))
+
+if [ $exit_code -ne 0 ]
+then
+    echo "test error!"
+    exit $exit_code
+else
+    echo "test passed!"
+    rm -f timer.dat training_set.xyz mbdecomp.log json_output.json
+fi

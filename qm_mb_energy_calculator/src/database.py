94811ac33f7b5b52a29ed0a057ae7b5884b1c906
Derek Tse <xiexr151e@gmail.com>
2018-05-01 17:30:26 -0700

    Further development on sqlite3 module

--- qm_mb_energy_calculator/src/database.py
+++ qm_mb_energy_calculator/src/database.py
@@ -2,15 +2,18 @@
 A module to figure out how to develop databases with sqlite3.
 """
 import sqlite3
+from datetime import datetime
 
+'''
 connect = sqlite3.connect("a.db")
 c = connect.cursor()
-#c.execute('''drop table if exists Molecules''')
-c.execute('''create table if not exists 
-    Molecules(mID int, mol blob, energies blob, updated date)''')
+#c.execute("drop table if exists Molecules")
+c.execute("create table if not exists 
+    Molecules(mID int, mol blob, energies blob, updated date)")
 c.execute("insert into Molecules values(2,0,0,0)")
 connect.commit()
 connect.close()
+'''
 
 def __init__(database_name):
     """
@@ -25,18 +28,53 @@ def __init__(database_name):
     Molecules(mID int, mol blob, energies blob, updated date)''')
     return cursor, connect
 
-def insert(cursor, table, obj_tuple):
+def query(cursor, table, obj_tuple, update=False):
+    """
+    Looks for an existing molecule configuration and do things with it
+    Input: The cursor object, the name of the table, and the configuration,
+           and a boolean for updating
+    Output: The data that shall be returned if no inserting/updating
+    """
+
+    cursor.execute('''select * from {} where mol=?'''.format(table),
+        (obj_tuple[1],))
+    data = cursor.fetchone()
+
+    if not data:
+        insert(cursor, table, obj_tuple)
+    elif update:
+        update(cursor, table, obj_tuple)
+    else:
+        print("Retrieving current data")
+        return data
+
+
+def insert(cursor, table, obj_tuple, update=False):
     """
     Inserts an object tuple into the table
     Input: The cursor object, the name of the table, and the values
            required by the tuple itself
     """
+    print("New configuration")
     cursor.execute('''insert into {} values{}'''.format(table, obj_tuple))
 
-def finalize(connection)
+def update(cursor, table, obj_tuple):
+    print("User intends to update energy calculation")
+    cursor.execute('''update {} set energies=?, updated=? where mol=?'''
+        .format(table), (obj_tuple[2], datetime.now(), obj_tuple[1],))
+
+
+def finalize(connection):
     """
     Ends the current session by saving and closing the database.
     Input: The connection to the database
     """
     connection.commit()
     connection.close()
+
+# All lines below functional
+'''
+cursor, connect = __init__("a.db")
+insert(cursor,"Molecules",(1,0,0,0), True)
+finalize(connect)
+'''

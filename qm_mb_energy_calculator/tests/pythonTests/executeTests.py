d8d654dcc94647e4f1e396298dca667716f0d510
EthanBullVulpe <ebullvul@ucsd.edu>
2018-05-15 16:47:27 -0700

    executeTests.py now makes detailed tests.log file documenting failures and errors

--- qm_mb_energy_calculator/tests/pythonTests/executeTests.py
+++ qm_mb_energy_calculator/tests/pythonTests/executeTests.py
@@ -11,10 +11,19 @@ result = unittest.TextTestRunner(verbosity=2).run(suite)
 # check if any failures or errors occured
 if len(result.failures) != 0 or len(result.errors) != 0:
     test_log = open("tests.log", "w+") # w+ opens file to write and creates if it didn't already exist
-    for failure in result.failures:
-        test_log.write(failure)
-    for error in result.errors:
-        test_log.write(error)
+    if len(result.failures)  != 0:
+        test_log.write("TEST FAILURES: \n\n")
+        for failure in result.failures:
+            test_log.write("TEST {} FAILED:\n".format(failure[0]))
+            test_log.write("Detailed Failure Information: \n")
+            test_log.write(failure[1] + "\n")
+        test_log.write("\n")
+    if len(result.errors)  != 0:
+        test_log.write("TEST ERRORS: \n\n")
+        for error in result.errors:
+            test_log.write("TEST {} ERRORED:\n".format(error[0]))
+            test_log.write("Detailed Error Information: \n")
+            test_log.write(error[1] + "\n")
     test_log.close();
 
     sys.exit(1) # exit with failure exit code

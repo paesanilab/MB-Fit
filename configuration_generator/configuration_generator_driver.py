8382f5577255bfcbbb9d4d3c5d9c1b4f5db42b73
Andreas Goetz <agoetz@sdsc.edu>
2018-06-27 16:55:04 -0700

    Convert tabs into 4 spaces

--- configuration_generator/configuration_generator_driver.py
+++ configuration_generator/configuration_generator_driver.py
@@ -17,54 +17,54 @@ import configuationloader
 import normalconfigurationgenerator
 
 if len(sys.argv) == 2:
-	parse_fail, name, input_geo, linear_geo, charge, multiplicity, random, num_configs, geometric, linear, method, basis = configuationloader.parse_config(sys.argv[1])
+    parse_fail, name, input_geo, linear_geo, charge, multiplicity, random, num_configs, geometric, linear, method, basis = configuationloader.parse_config(sys.argv[1])
 else:
-	parse_fail = True
-	print("Usage: automated-psi4.py [config.txt]")
-	
+    parse_fail = True
+    print("Usage: automated-psi4.py [config.txt]")
+    
 if parse_fail:
-	print("ERROR: The configuration file does not conform to the input format.")
+    print("ERROR: The configuration file does not conform to the input format.")
 else:
-	# filepaths
-	optimized_geometry_path = "inputs/" + name + "_psi4_" + method + "_" + basis + "_optimized.xyz"
-	normal_modes_path = "inputs/" + name + "_psi4_" + method + "_" + basis + "_normalmodes.dat"
+    # filepaths
+    optimized_geometry_path = "inputs/" + name + "_psi4_" + method + "_" + basis + "_optimized.xyz"
+    normal_modes_path = "inputs/" + name + "_psi4_" + method + "_" + basis + "_normalmodes.dat"
 
-	gcn_input_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".inp"
-	gcn_output_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".out"
+    gcn_input_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".inp"
+    gcn_output_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".out"
 
-	gcn_executor_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".sh"
+    gcn_executor_path = "gcn/" + name + "_psi4_" + method + "_" + basis + ".sh"
 
-	norm_config_path = "gcn/" + name + "_psi4_" + method + "_" + basis + "_configurations.xyz"
+    norm_config_path = "gcn/" + name + "_psi4_" + method + "_" + basis + "_configurations.xyz"
 
-	# psi4 Global Parameters
-	memory = "1GB"
-	num_threads = 2
-	output_path = "logs/" + name + "_psi4_" + method + "_" + basis + ".out"
+    # psi4 Global Parameters
+    memory = "1GB"
+    num_threads = 2
+    output_path = "logs/" + name + "_psi4_" + method + "_" + basis + ".out"
 
 
-	psi4.core.set_output_file(output_path, False)
-	psi4.set_memory(memory)
-	psi4.set_num_threads(num_threads)
+    psi4.core.set_output_file(output_path, False)
+    psi4.set_memory(memory)
+    psi4.set_num_threads(num_threads)
 
-	# Defining the molecule
-	geo = input_geo + "\n" + charge + " " + multiplicity
-	
-	mol = psi4.geometry(geo)
+    # Defining the molecule
+    geo = input_geo + "\n" + charge + " " + multiplicity
+    
+    mol = psi4.geometry(geo)
 
-	# Defining the computation models for steps 1 and 2
-	model = method + "/" + basis
-	
-	if linear_geo:
-		dim_null = 5
-	else:
-		dim_null = 6
-	
-	# Step 1
-	psi4helper.optimize_geometry(mol, model, optimized_geometry_path)
-	
-	# Step 2
-	psi4helper.frequency_calculations(mol, model, dim_null, normal_modes_path)
-	
-	# Step 3
-	normalconfigurationgenerator.generate(mol, gcn_input_path, gcn_output_path, optimized_geometry_path, normal_modes_path, norm_config_path, gcn_executor_path, dim_null, random, num_configs, geometric, linear)
-			
+    # Defining the computation models for steps 1 and 2
+    model = method + "/" + basis
+    
+    if linear_geo:
+        dim_null = 5
+    else:
+        dim_null = 6
+    
+    # Step 1
+    psi4helper.optimize_geometry(mol, model, optimized_geometry_path)
+    
+    # Step 2
+    psi4helper.frequency_calculations(mol, model, dim_null, normal_modes_path)
+    
+    # Step 3
+    normalconfigurationgenerator.generate(mol, gcn_input_path, gcn_output_path, optimized_geometry_path, normal_modes_path, norm_config_path, gcn_executor_path, dim_null, random, num_configs, geometric, linear)
+
--- configuration_generator/psi4helper.py
+++ configuration_generator/psi4helper.py
@@ -12,80 +12,80 @@ import sys
 
 # Converts an imaginary output from psi4 to a real output by splicing off the +j term (Sandra's code currently doesn't input imaginary numbers, so only the real frequency portion is maintained). I'm not sure if this is chemically/physically/mathematically correct, but it's what I'm doing.
 def treat_imaginary(contents):
-	string = str(contents)
-	
-	result = string.replace("(","")
-	result = result.replace(")","")
-	
-	plus_loc = result.index("+")
-	result = result[:plus_loc]
-	
-	return result
+    string = str(contents)
+    
+    result = string.replace("(","")
+    result = result.replace(")","")
+    
+    plus_loc = result.index("+")
+    result = result[:plus_loc]
+    
+    return result
 
 # Main Functions
 def optimize_geometry(molecule, model, optimized_geometry_path):
-	print("Optimizing geometry...")
+    print("Optimizing geometry...")
 
-	opt_formatter =  "{:< 16.10f}"
+    opt_formatter =  "{:< 16.10f}"
 
-	e = psi4.optimize(model, molecule=molecule)
-	
-	num_atoms = molecule.natom()
+    e = psi4.optimize(model, molecule=molecule)
+    
+    num_atoms = molecule.natom()
 
-	with open(optimized_geometry_path, 'w') as opt_geo_file:
-		opt_geo_file.write(str(num_atoms) + "\n")
-		opt_geo_file.write(str(e) + "\n")
-		
-		for i in range(num_atoms):
-			atom = molecule.symbol(i)
+    with open(optimized_geometry_path, 'w') as opt_geo_file:
+        opt_geo_file.write(str(num_atoms) + "\n")
+        opt_geo_file.write(str(e) + "\n")
+        
+        for i in range(num_atoms):
+            atom = molecule.symbol(i)
 
-			x = opt_formatter.format(molecule.x(i))
-			y = opt_formatter.format(molecule.y(i))
-			z = opt_formatter.format(molecule.z(i))
-			
-			opt_geo_file.write(atom + "\t" + x + " " + y + " " + z + "\n")
-			
-	print("")
-	
+            x = opt_formatter.format(molecule.x(i))
+            y = opt_formatter.format(molecule.y(i))
+            z = opt_formatter.format(molecule.z(i))
+            
+            opt_geo_file.write(atom + "\t" + x + " " + y + " " + z + "\n")
+            
+    print("")
+    
 def frequency_calculations(molecule, model, dim_null, normal_modes_path):
-	print("Determining normal modes and running frequency analysis...")
+    print("Determining normal modes and running frequency analysis...")
 
-	norm_formatter = "{:> 15.8f}"
+    norm_formatter = "{:> 15.8f}"
 
-	total_energy, wavefunc = psi4.frequency(model, molecule=molecule, return_wfn=True)
-	
-	vib_info_raw = wavefunc.frequency_analysis
-	vib_info = psi4.qcdb.vib.filter_nonvib(vib_info_raw)
-	
-	num_atoms = molecule.natom()
-	num_modes = 3*num_atoms - dim_null
+    total_energy, wavefunc = psi4.frequency(model, molecule=molecule, return_wfn=True)
+    
+    vib_info_raw = wavefunc.frequency_analysis
+    vib_info = psi4.qcdb.vib.filter_nonvib(vib_info_raw)
+    
+    num_atoms = molecule.natom()
+    num_modes = 3*num_atoms - dim_null
 
-	normal_out = ""
-	
-	frequencies = vib_info["omega"].data
-	reduced_masses = vib_info["mu"].data
-	
-	normal_modes = vib_info['x'].data
+    normal_out = ""
+    
+    frequencies = vib_info["omega"].data
+    reduced_masses = vib_info["mu"].data
+    
+    normal_modes = vib_info['x'].data
 
-	for i in range(1, 1 + num_modes):
-		index = i - 1
-		
-		normal_out += "normal mode: " + str(i) + "\n"
-		normal_out += treat_imaginary(frequencies[index]) + "\n"
-		normal_out += "red_mass = " + str(reduced_masses[index]) + "\n"
-		
-		for atom in range(num_atoms):
-			normal_out += norm_formatter.format(float(normal_modes[3 * atom + 0][index])) + "\t"
-			normal_out += norm_formatter.format(float(normal_modes[3 * atom + 1][index])) + "\t"
-			normal_out += norm_formatter.format(float(normal_modes[3 * atom + 2][index])) + "\n"
-		
-		normal_out += "\n"
+    for i in range(1, 1 + num_modes):
+        index = i - 1
+        
+        normal_out += "normal mode: " + str(i) + "\n"
+        normal_out += treat_imaginary(frequencies[index]) + "\n"
+        normal_out += "red_mass = " + str(reduced_masses[index]) + "\n"
+        
+        for atom in range(num_atoms):
+            normal_out += norm_formatter.format(float(normal_modes[3 * atom + 0][index])) + "\t"
+            normal_out += norm_formatter.format(float(normal_modes[3 * atom + 1][index])) + "\t"
+            normal_out += norm_formatter.format(float(normal_modes[3 * atom + 2][index])) + "\n"
+        
+        normal_out += "\n"
 
 
-	with open(normal_modes_path, 'w') as norm_file:
-		norm_file.write(normal_out)
+    with open(normal_modes_path, 'w') as norm_file:
+        norm_file.write(normal_out)
 
-	#print(vib_info)
+    #print(vib_info)
 
-	print("Normal mode/frequency analysis complete.")
-	print("")
+    print("Normal mode/frequency analysis complete.")
+    print("")

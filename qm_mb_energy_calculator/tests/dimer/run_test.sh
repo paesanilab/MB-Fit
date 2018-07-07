8970c9680365d438e325a20ba4deafc4b76a4db9
EthanBullVulpe <ebullvul@ucsd.edu>
2018-07-06 17:45:26 -0700

    some final fixes to tests in qm mb calc

--- qm_mb_energy_calculator/tests/dimer/run_test.sh
+++ qm_mb_energy_calculator/tests/dimer/run_test.sh
@@ -6,13 +6,13 @@ log=test.log
 
 python3 $driver
 
-ndiff -quiet -abserr 1.e-8 training_set.xyz expected/training_set.xyz > $log
+ndiff -quiet -abserr 1.e-5 training_set.xyz expected/training_set.xyz > $log
 exit_code=$?
 
-ndiff -quiet -abserr 1.e-8 mbdecomp.log expected/mbdecomp.log >> $log
+ndiff -quiet -abserr 1.e-5 mbdecomp.log expected/mbdecomp.log >> $log
 exit_code=$(($exit_code + $?))
 
-ndiff -quiet -abserr 1.e-8 -separators '[ \t,()]' json_output.json expected/json_output.json >> $log
+ndiff -quiet -abserr 1.e-5 -separators '[ \t,()]' json_output.json expected/json_output.json >> $log
 exit_code=$(($exit_code + $?))
 
 if [ $exit_code -ne 0 ]

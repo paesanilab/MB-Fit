0b61dcd610551a9df989f0bc41d9b0ecda9edf2b
EthanBullVulpe <ebullvul@ucsd.edu>
2018-07-09 12:07:13 -0700

    some final bugfixes

--- qm_mb_energy_calculator/src/database_reader.py
+++ qm_mb_energy_calculator/src/database_reader.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(0, "../../")
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../../")
 import sqlite3
 import pickle
 from database import Database

64e736f843d03eb9fce4bc775b8a16ef4a7f3ea6
EthanBullVulpe <ebullvul@ucsd.edu>
2018-07-31 18:47:19 -0700

    continued refactor of perl poly generation script

--- polynomial_generation/src/generate_poly.py
+++ polynomial_generation/src/generate_poly.py
@@ -1,31 +1,107 @@
 import sys
 import configparser
+import itertools
 
-def generate_poly(settings):
+def generate_poly(settings, input_file, order, output_path):
     
     # create configparser
     config = configparser.SafeConfigParser(allow_no_value=False)
     config.read(settings)
 
-    # order of polynomial to generate
-    poly_order = config["poly_generator"]["order"]
-    input_file = config["files"]["poly_in_path"]
-
     # open output files for polynomials, will automatically be closed by with open as syntax
-    with open(config["files"]["poly_path"] + "/vars.cpp", "w") as out_vars, open(config["files"]["poly_path"] + "/poly-direct.cpp", "w") as out_cpp, open(config["files"]["poly_path"] + "/poly-nogrd.maple", "w") as out_maple_nogrd, open(config["files"]["poly_path"] + "/poly-grd.maple", "w") as out_maple_grd, open(input_file, "r") as in_poly:
+    with open(output_path + "/vars.cpp", "w") as out_vars, open(output_path + "/poly-direct.cpp", "w") as out_cpp, open(output_path + "/poly-nogrd.maple", "w") as out_maple_nogrd, open(output_path + "/poly-grd.maple", "w") as out_maple_grd, open(output_path + "/poly.log", "w") as poly_log, open(input_file, "r") as in_poly:
         
         # read the add_molecule definitions to get a list of the fragments
         fragments = parse_fragments(in_poly)
 
+        # write number of fragments to log
+        poly_log.write("<> fragments({}) <>\n".format(len(fragments)))
+        poly_log.write("\n")
+
+        # write each fragment to log
+        for fragment in fragments:
+            poly_log.write(fragment + "\n")
+        poly_log.write("\n")
+
         # Parse the molecule names to determine and name the atoms in the system
 
+        # holds list of all atoms
         atom_names = []
+        # number of atoms in each fragment
         number_per_fragment = []
-        index_this_fragment = []
-        fragname = 'a'
+        # index of first atom in atom_names of each fragment
+        index_each_fragment = []
+        # letter to be part of atom_name, signifies which fragment this atom is a part of starts at 'a', will be incremented to 'b', etc
+        fragname = 97
         
+        # loop over each fragment
         for fragment in fragments:
-            pass
+
+            # counter to count how many atoms are in this fragment
+            number_of_atoms = 0
+
+            # loop thru all the atomic symbols in the fragment (skipping the numbers)
+            for symbol in fragment[::2]:
+                # loop once for each atom of this type as signified by the number immediately following the atomic symbol
+                for k in range(0, int(fragment[fragment.index(symbol) + 1])):
+                    # check if there are multiple atoms of this type in this fragment
+                    if int(fragment[fragment.index(symbol) + 1]) > 1:
+                        # add to atom_names in form Aa1
+                        atom_names.append(symbol + chr(fragname) + str(k + 1))
+                    else:
+                        # add to atom_names in form Aa
+                        atom_names.append(symbol + chr(fragname))
+                    
+                    # increment number of atoms
+                    number_of_atoms += 1
+
+            # add index of first atom in this fragment to index_each_fragment
+            index_each_fragment.append(sum(number_per_fragment))
+            # add number of atoms in this fragment to number_per)fragment
+            number_per_fragment.append(number_of_atoms)
+            
+            # increment fragname ('a' -> 'b', etc)
+            fragname += 1
+
+        # calculate total number of atoms in all fragments
+        num_atoms = len(atom_names)
+
+        # write number of atoms to log
+        poly_log.write("<> atoms ({}) <>\n".format(num_atoms))
+        poly_log.write("\n")
+
+        # write each atom to log
+        poly_log.write(":".join(atom_names) + "\n")
+        poly_log.write("\n")
+
+        # build the permutation group for each fragment
+
+        fragment_permutations = []
+        for fragment in fragments:
+            permutations = list(make_permutations([], fragment, 0))
+            
+            for permutation in permutations:
+                for index in range(len(permutation)):
+                    permutation[index] += index_each_fragment[fragments.index(fragment)]
+
+            fragment_permutations.append(permutations)
+
+        # construct total permutations from the fragments, including permutations of like fragments
+
+        atom_permutations = list(combine_permutations([], fragments, fragment_permutations))
+
+        # write permutation count to log file
+        poly_log.write("<> permutations ({}) <>\n".format(len(atom_permutations)))
+        poly_log.write("\n")
+
+        # write each permutation to log file
+        for permutation in atom_permutations:
+            poly_log.write(":".join(str(x) for x in permutation) + "\n")
+        poly_log.write("\n")
+
+        # read variables from input file
+        
+        
 
 def parse_fragments(input_file):
     fragments = []
@@ -35,9 +111,36 @@ def parse_fragments(input_file):
         line = input_file.readline()
 
     return fragments
+
+def make_permutations(existing_permutation, fragment, index):
+    # 2nd letter of fragment is next atom count
+    try:
+        count = int(fragment[1])
+    except IndexError:
+        yield existing_permutation
+        raise StopIteration
+
+    permutations = itertools.permutations(range(count))
+    
+    for permutation in permutations:
+        yield from make_permutations(existing_permutation + [i + index for i in permutation], fragment[2:], index + count)
+
+def combine_permutations(existing_permutation, fragments, fragment_permutations):
+
+    if len(fragments) == 0:
+        yield existing_permutation
+        raise StopIteration
+
+    for permutation in fragment_permutations[0]:
+        yield from combine_permutations(existing_permutation + permutation, fragments[1:], fragment_permutations[1:])
+
+    for i in range(1, len(fragments)):
+        if fragments[i] == fragments[0]:
+            for permutation in fragment_permutations[i]:
+                yield from combine_permutations(existing_permutation + permutation, fragments[1:i] + [fragments[0]] + fragments[i + 1:], fragment_permutations[1:i] + [fragments[0]] + fragments[i + 1:])
    
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python generate_poly.py settings.ini")
+    if len(sys.argv) != 5:
+        print("Usage: python generate_poly.py settings.ini input_file order output_path")
         sys.exit(1)
-    generate_poly(sys.argv[1])
+    generate_poly(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])

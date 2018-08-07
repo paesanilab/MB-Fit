da1d90d820932fa00040a2fa52cec7dc240639a7
EthanBullVulpe <ebullvul@ucsd.edu>
2018-08-07 13:31:52 -0700

    cleaned up recursive methods in generate_poly

--- polynomial_generation/src/generate_poly.py
+++ polynomial_generation/src/generate_poly.py
@@ -77,18 +77,18 @@ def generate_poly(settings, input_file, order, output_path):
         # build the permutation group for each fragment
 
         fragment_permutations = []
-        for fragment in fragments:
-            permutations = list(make_permutations([], fragment, 0))
+        for frag_index, fragment in enumerate(fragments):
+            permutations = list(make_permutations(fragment))
             
             for permutation in permutations:
                 for index in range(len(permutation)):
-                    permutation[index] += index_each_fragment[fragments.index(fragment)]
+                    permutation[index] += index_each_fragment[frag_index]
 
             fragment_permutations.append(permutations)
 
         # construct total permutations from the fragments, including permutations of like fragments
 
-        atom_permutations = list(combine_permutations([], fragments, fragment_permutations))
+        atom_permutations = list(combine_permutations(fragments, fragment_permutations))
 
         # write permutation count to log file
         poly_log.write("<> permutations ({}) <>\n".format(len(atom_permutations)))
@@ -117,66 +117,128 @@ def generate_poly(settings, input_file, order, output_path):
         for degree in range(1, order + 1):
             poly_log.write("<> {} degree <>\n".format(degree))
             poly_log.write("\n")
-            monomials = list(generate_monomials(list(0 for x in range(len(variables))), degree, 0))
+            monomials = list(generate_monomials(len(variables), degree))
             poly_log.write("{} possible {} degree monomials\n".format(len(monomials), degree))
         
         
-
 def parse_fragments(input_file):
+    """
+    Reads the add_molecule statements from the input file
+    """
+
     fragments = []
+
+    # get a line from the input file
     line = input_file.readline()
+
+    # continue reading lines as long as they begin with add_m
     while line[:5] == "add_m":
+        
+        # parse the fragment from the line
         fragments.append(line[line.index("['") + 2:line.index("']")])
+
+        # get a new line from the input file
         line = input_file.readline()
 
     return fragments
 
 def parse_variables(input_file):
+    """
+    Reads the add variable statements from the input file
+    """
+
+    # loop thru all the rest of the lines in the input file, constructing variable objects
     for line in input_file.readlines():
         yield Variable(line)
         
 class Variable(object):
+    """
+    Holds all information relevent to a single variable
+    """
     def __init__(self, line):
+        # remove all the extra information before and after the brackets
         line = line[line.index("[") + 1:line.index("]")]
+
+        # parse the line into this Variable's fields
         self.atom1_name, self.atom1_fragment, self.atom2_name, self.atom2_fragment, self.category = line.replace("'", "").replace(" ", "").split(",")
     
 
-def make_permutations(existing_permutation, fragment, index):
-    # 2nd letter of fragment is next atom count
+def make_permutations(fragment):
+    """
+    Takes in a fragment and constructs all permutations of that fragment
+    """
+
     try:
+        # read the 2nd letter of the fragment, the atom count. (For instance 2 in A2B3)
         count = int(fragment[1])
     except IndexError:
-        yield existing_permutation
-        raise StopIteration
+        # if the fragment string is empty, then this fragment has no permutations
+        yield []
+        return
 
+    # get all possible permutations of the number of atoms indicated by count
     permutations = itertools.permutations(range(count))
     
+    # loop thru each of the permutations
     for permutation in permutations:
-        yield from make_permutations(existing_permutation + [i + index for i in permutation], fragment[2:], index + count)
+        
+        # yield from the generator created by concatinating the permutation of the first atom type in the fragment with the permutations created by a recursive call on the rest of the fragment
+        # increase each item in the lists given by the recursive call equal to the number of atoms of the first type in the fragment
+        yield from (list(permutation) + [count + i for i in perm] for perm in make_permutations(fragment[2:]))
 
-def combine_permutations(existing_permutation, fragments, fragment_permutations):
-
+def combine_permutations(fragments, fragment_permutations):
+    """
+    Takes a list of fragments ("A1B2", "A1B1C1", etc) and a list of the permutations of those fragments and yields permutations of the molecules as a whole
+    """
+    
+    # if there are no fragments, then there are no permutations for this molecule
     if len(fragments) == 0:
-        yield existing_permutation
-        raise StopIteration
+        yield []
+        return
 
+    # loop thru every permutation of the first fragment
     for permutation in fragment_permutations[0]:
-        yield from combine_permutations(existing_permutation + permutation, fragments[1:], fragment_permutations[1:])
 
+        # yield from the first fragment's permutation concatinated with the permutations of the rest of the molecule
+        yield from (permutation + perm for perm in combine_permutations(fragments[1:], fragment_permutations[1:]))
+
+    # loop thru every other fragmanet in the molecule
     for i in range(1, len(fragments)):
+        
+        # check if the current fragment (0) is the same as another one
         if fragments[i] == fragments[0]:
+
+            # loop thru every permutation of the fragment that is the same as the current one
             for permutation in fragment_permutations[i]:
-                yield from combine_permutations(existing_permutation + permutation, fragments[1:i] + [fragments[0]] + fragments[i + 1:], fragment_permutations[1:i] + [fragments[0]] + fragments[i + 1:])
 
-def generate_monomials(existing_powers, degree, min_index):
+                # yield from that fragment's permutation concatinated with the permuations of the rest of the molecule with the current fragment (0) swapped into the position of the fragment it is the same as.
+                yield from (permutation + perm for perm in combine_permutations(fragments[1:i] + [fragments[0]] + fragments[i + 1:], fragment_permutations[1:i] + [fragment_permutations[0]] + fragment_permutations[i + 1:]))
+
+def generate_monomials(number_of_vars, degree):
+    """
+    Given a number of variables and a degree, generates all possible monomials of that degree.
+
+    yeilds arrays of length number_of_vars with entries adding to degree
+    """
+
+    # if number of vars is 1, then the only possible monomial is a monomial with 1 variable with the given degree
+    if number_of_vars == 1:
+        yield [degree]
+        return
+
+    # if degree is 0, then the only possible monomial is a monomial with all 0 degrees with the given number of variables
     if degree == 0:
-        yield existing_powers
-        raise StopIteration
+        yield [0 for i in range(number_of_vars)]
+        return
 
-    for i in range(min_index, len(existing_powers)):
-        new_powers = existing_powers[:]
-        new_powers[i] += 1
-        yield from generate_monomials(new_powers, degree - 1, i)
+    # loop over all possible first non-zero terms
+    for v in range(number_of_vars):
+
+        # loop over all possible degree values of the first non-zero term
+        for d in range(1, degree + 1):
+
+            # yield from the list created by all zeros before the first non-zero term, then the first non-zero term, then each result of the recursive call on all terms after the first non-zero term with degree equal to degree minus the degree of the first non-zero term
+            yield from ([0 for i in range(v)] + [d] + monomial for monomial in generate_monomials(number_of_vars - v - 1, degree - d))
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:

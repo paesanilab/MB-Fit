61144227b4b3c42e5e1b7fd24a415294c6d34e93
EthanBullVulpe <ebullvul@ucsd.edu>
2018-08-06 17:14:25 -0700

    started 2b training set generation, really don't know how to do it

--- qm_mb_energy_calculator/src/training_set_generator.py
+++ qm_mb_energy_calculator/src/training_set_generator.py
@@ -7,9 +7,20 @@ from database import Database
 
 def generate_1b_training_set(settings, database_name, output_path, molecule_name, method, basis, cp, tag):
     """
-    Creates a training set file from the calculated energies in a database
-    
-    Currently ONLY WORKS FOR MONOMERS
+    Creates a training set file from the calculated energies in a database.
+
+    Args:
+        settings    - .ini file with all relevent settings information
+        database_name - filepath to database file
+        output_path - path to file to write training set to
+        molecule_name - the name of the molecule to generate a training set for
+        method      - use energies calculated with this method. Use % for any method
+        basis       - use energies calculated with this basis. Use % for any basis
+        cp          - use energies calculated with this cp. Use 0 for False, 1 for True, or % for any cp
+        tag         - use energies marked with this tag. Use % for any tag
+
+    Return:
+        None
     """
     
     # add .db to database name if it doesn't already end in .db 
@@ -17,11 +28,12 @@ def generate_1b_training_set(settings, database_name, output_path, molecule_name
         print("Database name \"{}\" does not end in database suffix \".db\". Automatically adding \".db\" to end of database name.".format(database_name))
         database_name += ".db"
     
+    # open the database
     database = Database(database_name)
 
     print("Creating a fitting input file from database {} into file {}".format(database_name, output_path))
 
-    # get list of all [molecule, energies] pairs calculated in the database where energies is a list: [E0, E1, E2, E01, E12, E02, E012] with N/A energies missing
+    # get list of all [molecule, energies] pairs calculated in the database
     molecule_energy_pairs = list(database.get_energies(molecule_name, method, basis, cp, tag))
 
     # if there are no calculated energies, error and exit
@@ -30,15 +42,79 @@ def generate_1b_training_set(settings, database_name, output_path, molecule_name
         database.close()
         exit(1)
     
-    # temporary code only works for 1b
-    
     # find the optimized geometry energy from the database
     try:
         opt_energies = list(database.get_optimized_energies(molecule_name, method, basis, cp, tag))[0][1]
     except IndexError:
-        raise ValueError("No optimized geometry in database. Terminating training set generation") from None
+        raise ValueError("No optimized geometry in database. Terminating training set generation.") from None
 
-    num_bodies = math.log(len(opt_energies) + 1, 2)
+    # open output file for writing
+    output = open(output_path, "w")
+
+    # loop thru each molecule, energy pair
+    for molecule_energy_pair in molecule_energy_pairs:
+        molecule = molecule_energy_pair[0]
+        energies = molecule_energy_pair[1]
+
+        # write the number of atoms to the output file
+        output.write(str(molecule.get_num_atoms()) + "\n")
+
+        # monomer interaction energy
+        output.write(str((float(energies[0]) - float(opt_energies[0])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
+	
+        output.write("\n")
+
+        # write the molecule's atoms' coordinates to the xyz file
+        output.write(molecule.to_xyz() + "\n")
+
+    database.close()
+
+
+def generate_2b_training_set(settings, database_name, output_path, molecule_name, monomer_1_name, monomer_2_name, method, basis, cp, tag):
+    """"
+    Creates a training set file from the calculated energies in a database.
+
+    Args:
+        settings    - .ini file with all relevent settings information
+        database_name - filepath to database file
+        output_path - path to file to write training set to
+        molecule_name - the name of the molecule to generate a training set for
+        monomer_1_name - the name of the first monomer in the dimer NOTHING WORKS!
+        monomer_2_name - the name of the second monomer in the dimer
+        method      - use energies calculated with this method. Use % for any method
+        basis       - use energies calculated with this basis. Use % for any basis
+        cp          - use energies calculated with this cp. Use 0 for False, 1 for True, or % for any cp
+        tag         - use energies marked with this tag. Use % for any tag
+
+    Return:
+        None
+    """
+    
+    # add .db to database name if it doesn't already end in .db 
+    if database_name[-3:] != ".db":
+        print("Database name \"{}\" does not end in database suffix \".db\". Automatically adding \".db\" to end of database name.".format(database_name))
+        database_name += ".db"
+    
+    # open the database
+    database = Database(database_name)
+
+    print("Creating a fitting input file from database {} into file {}".format(database_name, output_path))
+
+    # get list of all [molecule, energies] pairs calculated in the database
+    molecule_energy_pairs = list(database.get_energies(molecule_name, method, basis, cp, tag))
+
+    # if there are no calculated energies, error and exit
+    if len(molecule_energy_pairs) == 0:
+        print("No completed energy entries to generate a training set.")
+        database.close()
+        exit(1)
+    
+    # find the optimized geometry energy of the two monomers from the database
+    try:
+        monomer_1_opt_energies = list(database.get_optimized_energies(monomer_1_name, method, basis, cp, tag))[0][1]
+        monomer_2_opt_energies = list(database.get_optimized_energies(monomer_2_name, method, basis, cp, tag))[0][1]
+    except IndexError:
+        raise ValueError("No optimized geometry in database. Terminating training set generation") from None
 
     # open output file for writing
     output = open(output_path, "w")
@@ -50,25 +126,14 @@ def generate_1b_training_set(settings, database_name, output_path, molecule_name
         # write the number of atoms to the output file
         output.write(str(molecule.get_num_atoms()) + "\n")
 
-        # if 1 body
-        if num_bodies == 1:
+        interaction_energy = (energies[2] - energies[1] - energies[1]) * constants.au_to_kcal # covert Hartrees to kcal/mol
+        monomer1_energy_formation = (energies[0] - monomer_1_opt_energies[0]) * constants.au_to_kcal # covert Hartrees to kcal/mol
+        monomer2_energy_formation = (energies[1] - monomer_2_opt_energies[0]) * constants.au_to_kcal # covert Hartrees to kcal/mol
 
-            # monomer interaction energy
-            output.write(str((float(energies[0]) - float(opt_energies[0])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
+        binding_energy = interaction_energy - monomer1_energy_formation - monomer2_energy_formation
 
-        # if 2 bodies
-        elif num_bodies == 2:
-
-            # dimer interaction energy
-            output.write(str((float(energies[2]) - float(opt_energies[2])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
-
-            # dimer binding energy
-            output.write(str((float(energies[2] - energies[0] - energies[1]) - float(opt_energies[2] - opt_energies[0] - opt_energies[1])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
-
-            # one body energies
-            output.write(str((float(energies[0]) - float(opt_energies[0])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
-            output.write(str((float(energies[1]) - float(opt_energies[1])) * constants.au_to_kcal) + " ") # covert Hartrees to kcal/mol
-
+        # monomer interaction energy
+        output.write("{} {} {} {}".format(binding_energy, interaction_energy, monomer1_energy_formation, monomer2_energy_formation))
 	
         output.write("\n")
 
@@ -76,6 +141,7 @@ def generate_1b_training_set(settings, database_name, output_path, molecule_name
         output.write(molecule.to_xyz() + "\n")
 
     database.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:

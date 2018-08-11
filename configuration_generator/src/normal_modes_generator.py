d356515dd4dba0ed42f0ba69e4c9358f8c620ead
EthanBullVulpe <ebullvul@ucsd.edu>
2018-08-10 18:19:15 -0700

    fixed a bug in normal_modes_generator.py

--- configuration_generator/src/normal_modes_generator.py
+++ configuration_generator/src/normal_modes_generator.py
@@ -14,7 +14,7 @@ def generate_normal_modes(settings, geo_path, normal_modes_file):
     with open(geo_path, "r") as geo_file:
         molec = molecule.Molecule(geo_file.read())
     
-    normal_modes, frequencies, red_masses = qcalc.frequencies(molec, config)
+    normal_modes, frequencies, red_masses = qcalc.frequencies(molec, {"log_name": config["files"]["log_path"]}, config)
     dim_null = 3 * molec.num_atoms - len(normal_modes)
     
     output_writer.write_normal_modes(normal_modes, frequencies, red_masses, normal_modes_file)

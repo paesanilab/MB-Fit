4c7a18a7abd9ebb5fa0f3d79e11de11affbf3841
EthanBullVulpe <ebullvul@ucsd.edu>
2018-09-26 14:33:42 -0700

    started work on python normal distribution configuration generator

--- potential_fitting/configurations/normal_modes_generator.py
+++ potential_fitting/configurations/normal_modes_generator.py
@@ -44,7 +44,7 @@ def write_normal_modes(normal_modes, frequencies, red_masses, normal_modes_path)
         
         normal_out += "normal mode: " + str(i) + "\n"
         normal_out += "frequency = " + freq_formatter.format(frequencies[index]) + "\n"
-        normal_out += "red_mass = " + mass_formatter.format(red_masses[index]) + "\n"
+        normal_out += "reduced mass = " + mass_formatter.format(red_masses[index]) + "\n"
         
         # for each atom in the molecule
         for atom in range(len(geo)):

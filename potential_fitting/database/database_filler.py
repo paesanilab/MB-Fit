e6f449a15048a41e8f75020d909c21abe8aedc48
Marc Riera <mrierari@ucsd.edu>
2019-01-27 13:41:37 -0800

    Splitted database filling. Just for testing purposes for now.

--- potential_fitting/database/database_filler.py
+++ potential_fitting/database/database_filler.py
@@ -52,6 +52,72 @@ def fill_database(settings_path, database_path):
 
         print("\nFilling of database {} successful".format(database_path))
 
+def generate_inputs_from_database(settings_path, database_path):
+
+    # open the database
+    with Database(database_path) as database:
+
+        print("Starting input creation of database {}".format(database_path))
+        # parse settings file
+        settings = SettingsReader(settings_path)
+
+        counter = 0
+
+        for calculation in database.missing_energies():
+
+            counter += 1
+            print_progress(counter)
+
+            # create input
+            calculator.generate_input(calculation.molecule, calculation.fragments, calculation.method + "/" + calculation.basis, calculation.cp, settings)
+
+
+        print("\nInput creation of database {} successful".format(database_path))
+
+def run_missing_calculations(settings_path, database_path):
+
+    # open the database
+    with Database(database_path) as database:
+
+        print("Running missing energies in database {}".format(database_path))
+        # parse settings file
+        settings = SettingsReader(settings_path)
+
+        counter = 0
+
+        for calculation in database.missing_energies():
+            counter += 1
+            print_progress(counter)
+
+            # Run this calculation
+            calculator.run_calculation(calculation.molecule, calculation.fragments, calculation.method + "/" + calculation.basis, calculation.cp, settings)
+
+def retrieve_energies(settings_path, database_path):
+
+    # open the database
+    with Database(database_path) as database:
+
+        print("Retrieving energies to database {}".format(database_path))
+        # parse settings file
+        settings = SettingsReader(settings_path)
+
+        counter = 0
+
+        for calculation in database.missing_energies():
+            counter += 1
+            print_progress(counter)
+
+            try:
+                # calculate the missing energy
+                energy = calculator.retrieve_energy(calculation.molecule, calculation.fragments, calculation.method + "/" + calculation.basis, calculation.cp, settings)
+                # update the energy in the database
+                database.set_energy(calculation.job_id, energy, "some/log/path")
+            except LibraryCallError:
+                database.set_failed(calculation.job_id, "some/log/path")
+
+            # save changes to the database
+            database.save()
+
 def print_progress(counter):
     """
     Prints the given number to the console. Followed by a newline if the number is divisible by 10.

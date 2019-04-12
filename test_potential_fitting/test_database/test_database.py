e9556a04b99fb7299b5cfd5c6e2d1e7207bcc4e6
EthanBullVulpe <ebullvul@ucsd.edu>
2019-04-12 16:02:28 -0700

    started database testing module

--- test_potential_fitting/test_database/test_database.py
+++ test_potential_fitting/test_database/test_database.py
@@ -1,9 +1,24 @@
-import unittest
+import unittest, os
+
+from potential_fitting.database import Database
+
 
 class TestDatabase(unittest.TestCase):
 
-    def test_get_name(self):
-        pass
+    def setUp(self):
+        self.config = os.path.join(os.path.dirname(os.path.abspath(__file__)), "database.ini)")
+        self.database = Database(self.config)
+
+    def tearDown(self):
+        self.database.annihilate(confirm="confirm")
+        self.database.close()
+
+    def test_set_and_get_batch_size(self):
+        self.database.set_batch_size(8)
+        self.assertEqual(self.database.get_batch_size(), 8)
+
+        self.database.set_batch_size(15)
+        self.assertEqual(self.database.get_batch_size(), 15)
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestAtom)

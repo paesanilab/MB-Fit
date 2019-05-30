84a04a350a2d6674f9455ab62286f9ba9cda25ad
EthanBullVulpe <ebullvul@ucsd.edu>
2019-05-30 15:30:24 -0700

    added wrappers for reset_dispatched and reset_failed

--- potential_fitting/database/database_cleaner.py
+++ potential_fitting/database/database_cleaner.py
@@ -1,14 +1,15 @@
 # local module imports
 from .database import Database
 
-def clean_database(database_config_path, settings_path):
+def clean_database(database_config_path, settings_path, tags*):
     """
-    Sets all running calculations back to pending in the given database.
+    Sets all dispatched calculations back to pending in the given database.
 
     Args:
         database_config_path - .ini file containing host, port, database, username, and password.
                     Make sure only you have access to this file or your password will be compromised!
         settings_path       - Local path to ".ini" file containing all relevent settings.
+        tags                - Reset calculations with one of these tags.
 
     Returns:
         None.
@@ -16,10 +17,22 @@ def clean_database(database_config_path, settings_path):
 
     with Database(config_path) as database:
 
-        database.clean()
+        database.reset_dispatched(*tags)
 
-def reset_database(settings_path, database_path):
+def reset_database(settings_path, database_path, tags*):
+    """
+    Sets all failed calculations back to pending in the given database.
+
+    Args:
+        database_config_path - .ini file containing host, port, database, username, and password.
+                    Make sure only you have access to this file or your password will be compromised!
+        settings_path       - Local path to ".ini" file containing all relevent settings.
+        tags                - Reset calculations with one of these tags.
+
+    Returns:
+        None.
+    """
+
     with Database(database_path) as database:
 
-        database.reset()
-
+        database.reset_failed(*tags)

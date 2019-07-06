136e404b4c3c92ccec7d74e05aba9cff53161731
EthanBullVulpe <ebullvul@ucsd.edu>
2019-07-05 18:16:04 -0700

    fixed bug where virtual sites would dissapear from get_config_data.py

--- potential_fitting/fitting/get_config_data.py
+++ potential_fitting/fitting/get_config_data.py
@@ -52,7 +52,7 @@ def make_config(settings_file, molecule_in, config_path, *geo_paths, distance_be
 
     fragments = ["".join([atom_type.get_atom_in() for atom_type in frag.get_atom_types()]) for frag in parser.get_fragments()]
 
-    molecule_in = "_".join(fragments)
+    molecule_in = "_".join(["".join([atom_type.get_atom_in() for atom_type in frag.get_atom_and_virtual_site_types()]) for frag in parser.get_fragments()])
 
     if len(geo_paths) != len(fragments):
         raise InconsistentValueError("number of geometries", "number of fragments", len(geo_paths), len(fragments), "number of geometries must be equal to the number of fragments in the A3B2_A3B2 type input")

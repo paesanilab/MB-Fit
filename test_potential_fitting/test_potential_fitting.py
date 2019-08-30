18f4d5bc3d8dbe93d99bf5bb2f84e2aae812a9dc
EthanBullVulpe <ebullvul@ucsd.edu>
2019-08-29 17:25:00 -0700

    re-enabled all tests

--- test_potential_fitting/test_potential_fitting.py
+++ test_potential_fitting/test_potential_fitting.py
@@ -2,4 +2,3 @@ import unittest
 from . import test_molecule, test_database, test_calculator, test_utils, test_polynomials, test_fitting
 
 suite = unittest.TestSuite([test_molecule.suite, test_database.suite, test_calculator.suite, test_utils.suite, test_polynomials.suite, test_fitting.suite])
-suite = unittest.TestSuite([test_polynomials.suite])

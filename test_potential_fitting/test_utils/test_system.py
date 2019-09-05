bddc4360ea1645f38999cc0521aa435e7f2a0a74
EthanBullVulpe <ebullvul@ucsd.edu>
2019-09-04 18:51:20 -0700

    in_file.txt and out_file.txt no longer are written to the home directory

--- test_potential_fitting/test_utils/test_system.py
+++ test_potential_fitting/test_utils/test_system.py
@@ -1,31 +1,39 @@
-import unittest
+import unittest, os
 
-from potential_fitting.utils import system
+from potential_fitting.utils import system, files
 from potential_fitting.exceptions import CommandExecutionError, CommandNotFoundError
 
 class TestSystem(unittest.TestCase):
 
+    def setUpClass():
+        TestSystem.bad_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output", "bad_file.txt")
+        TestSystem.in_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output", "in_file.txt")
+        TestSystem.out_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output", "out_file.txt")
+
     def test_call(self):
 
+        files.init_file(TestSystem.in_file)
+        files.init_file(TestSystem.out_file)
+
         with self.assertRaises(CommandExecutionError):
-            system.call("cat", "bad_file.txt")
+            system.call("cat", TestSystem.bad_file)
 
         with self.assertRaises(CommandNotFoundError):
             system.call("bad_command")
 
-        with open("out_file.txt", "w") as out_file:
+        with open(TestSystem.out_file, "w") as out_file:
             system.call("echo", "echo echo  echo   echo   echo    echo", out_file=out_file)
 
-        with open("out_file.txt", "r") as out_file:
+        with open(TestSystem.out_file, "r") as out_file:
             self.assertEqual(out_file.read(), "echo echo  echo   echo   echo    echo\n")
 
-        with open("in_file.txt", "w") as in_file:
+        with open(TestSystem.in_file, "w") as in_file:
             in_file.write("cat!\n")
 
-        with open("in_file.txt", "r") as in_file, open("out_file.txt", "w") as out_file:
+        with open(TestSystem.in_file, "r") as in_file, open(TestSystem.out_file, "w") as out_file:
             system.call("cat", in_file=in_file, out_file=out_file)
 
-        with open("out_file.txt", "r") as out_file:
+        with open(TestSystem.out_file, "r") as out_file:
             self.assertEqual(out_file.read(), "cat!\n")
 
 

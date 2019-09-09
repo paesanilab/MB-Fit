9b88cf129cd79eca17b99a2cbe3219574568b400
EthanBullVulpe <ebullvul@ucsd.edu>
2019-09-09 16:23:48 -0700

    Removed old files

--- potential_fitting/database/__init__.py
+++ potential_fitting/database/__init__.py
@@ -7,8 +7,6 @@ from .database_filler import generate_inputs_from_database
 from .database_filler import run_missing_calculations
 from .database_filler import retrieve_energies
 from .database_initializer import initialize_database
-from .database_job_maker import make_all_jobs, write_job
-from .database_job_reader import read_all_jobs, read_job
 from .training_set_generator import generate_1b_training_set, generate_2b_training_set, generate_training_set
 from .job_handler import JobHander
 from .psi4_job_handler import Psi4JobHander

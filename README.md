c34ae52ccfb2fadbb1daf1d37a62c13e83c22d1c
EthanBullVulpe <ebullvul@ucsd.edu>
2020-01-10 15:45:05 -0800

    Fixed a bug where the ratio (Effective Volume / Free Volume) would not be  24 raised to the power of (4/3) when calculating effective polarizabilities.
    Closes #299.

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ Most recent changes at the top
 Version v0.2.3 (Under development, not released.)
 * Fixed a bug where retrieve_best_fit() would think some converged fits failed
 to converge.
+* Fixed a bug where the ratio (Effective Volume / Free Volume) would not be
+raised to the power of (4/3) when calculating effective polarizabilities.
 
 Version v0.2.2
 * Fixed a bug where Qchem optimizations would crash when using the
--- potential_fitting/fitting/generate_config_file.py
+++ potential_fitting/fitting/generate_config_file.py
@@ -363,7 +363,7 @@ def get_chg_pol_from_qchem_output(qchem_out_path, fragment, atomic_symbols, use_
                     free_polarizability = constants.symbol_to_ccsdt_free_polarizability(atomic_symbols[atom_count])
 
                 # calculate the effective polarizability
-                effective_polarizability = free_polarizability * effective_volume / free_volume
+                effective_polarizability = free_polarizability * (effective_volume / free_volume) ** (4.0/3.0)
 
                 # add the effective polarizability to the corresponding list in the dictionary
                 effective_polarizability_dictionary[atom_type].append(effective_polarizability)

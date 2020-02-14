53f0c3c2c826bfa8090c7234d90262229f19a9f1
EthanBullVulpe <ebullvul@ucsd.edu>
2020-02-13 17:01:10 -0800

    fixed typo in settings access and change Evaluator to take only 1 settings file

--- potential_fitting/fitting/eval.py
+++ potential_fitting/fitting/eval.py
@@ -9,9 +9,9 @@ class Evaluator:
         self.settings = settings
         self.path_to_eval_file = path_to_eval_file
 
-    def eval(self, path_to_training_set_file, settings):
+    def eval(self, path_to_training_set_file):
 
-        correlation_file_path = files.init_file(self.settings.get("files", "logs"), os.path.join("eval_correlation.dat"))
+        correlation_file_path = files.init_file(self.settings.get("files", "log_path"), os.path.join("eval_correlation.dat"))
 
         with open(correlation_file_path, "w") as correlation_file:
             system.call(self.path_to_eval_file, path_to_training_set_file, out_file=correlation_file)
@@ -23,7 +23,7 @@ class Evaluator:
                 if not line.startswith("#"):
                     fit_energies.append(float(line.split()[1]))
 
-        training_set = TrainingSet.get_training_set_from_xyz_file(path_to_training_set_file, settings, energy_names=["weight_energy", "ref_energy"])
+        training_set = TrainingSet.get_training_set_from_xyz_file(path_to_training_set_file, self.settings, energy_names=["weight_energy", "ref_energy"])
 
         training_set.add_energies("fit_energies", fit_energies)
 

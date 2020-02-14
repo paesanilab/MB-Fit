2a60869d8ce65c120fbd11999882998aff3a2da7
EthanBullVulpe <ebullvul@ucsd.edu>
2020-02-14 15:40:58 -0800

    fixed a bug where delete_complete_calculations would get passed as a tag

--- potential_fitting/database/database_cleaner.py
+++ potential_fitting/database/database_cleaner.py
@@ -70,7 +70,7 @@ def delete_calculations(settings_path, database_config_path, configurations_path
 
     with Database(database_config_path) as database:
 
-        database.delete_calculations(molecules, method, basis, cp, *tags, delete_complete_calculations)
+        database.delete_calculations(molecules, method, basis, cp, *tags, delete_complete_calculations=delete_complete_calculations)
 
 
 def delete_all_calculations(settings_path, database_config_path, molecule_name, method, basis, cp, *tags, delete_complete_calculations = False):

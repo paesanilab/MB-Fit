9631bc3135789ce9ee2c381305856ffd18a81ff3
EthanBullVulpe <ebullvul@ucsd.edu>
2020-06-22 18:11:02 -0700

    finished database setup guide

---
+++ database_setup.py
@@ -0,0 +1,16 @@
+import sys
+
+from potential_fitting.database import Database
+
+
+
+
+if(len(sys.argv) != 2):
+    print("Usage:")
+    print("{} <database config>".format(sys.argv[0]))
+    exit(1)
+
+database_config = sys.argv[1]
+
+with Database(database_config) as database:
+    database.create()
